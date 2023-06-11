5c1abde8a223fb7057104bebc25a6cbf40125fd7
Ken Chigozie <kenronkwo@gmail.com>
2023-06-11 12:31:09 +0100

    My Portfolio Website

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,14 +6,14 @@ function Form() {
     <div className='form'>
         <form action='https://getform.io/f/bf559128-abe6-4906-890d-29c673357287' method='POST' >
             <label htmlFor="nam">Name</label>
-            <input type="text" name="Name" id="nam" required />
-            <label htmlFor="">Email</label>
-            <input type="email" name="Email" id="" required/>
-            <label htmlFor="">Subject</label>
-            <input type="text" name="Subject" id="" />
-            <label htmlFor="" required>Message</label>
-           <textarea rows={6} name='Your Message' placeholder='Type your Message here'/>
-           <button className='btn'>Submit</button>
+            <input type="text" name="Name"  required id='nam' />
+            <label htmlFor='email'>Email</label>
+            <input type="email" name="Email"  required id='email '/>
+            <label htmlFor='subject'>Subject</label>
+            <input type="text" name="Subject"  id='subject'/>
+            <label htmlFor="Subject" >Message</label>
+           <textarea rows={6} name='Your Message' placeholder='Type your Message here' required/>
+           <button className='btn' >Submit</button>
             
         </form>
     </div>
